80e6f2378310255f72190da3e75d81c132c799f2
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-10-20 17:38:15 -0400

    created deposit method, set account_number equal to random 8-digit number, set balance equal to 0

--- bankAccount.py
+++ bankAccount.py
@@ -1,3 +1,10 @@
+from random import randint
 class BankAccount():
-    def __init__(self, full_name, account_number, balance):
-        pass
+    def __init__(self, full_name, account_number = randint(00000000,99999999), balance = 0):
+        self.full_name = full_name
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f'Amount Deposited: ${amount}\nNew Balance: {self.balance}')
