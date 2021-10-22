07f6401ffef43b727fdc6b55fffbff565c0d2ee0
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-10-22 17:36:11 -0400

    uncommented tests & deleted redundent code prior to auto-adding accnts to global bank list

--- bankAccount.py
+++ bankAccount.py
@@ -13,11 +13,14 @@ class BankAccount():
                 self.account_number *= 10
             print(f'Your account number must be 8 digits long, here is your new account number: {self.account_number}')
     
+
         if self.type != 'checking' and self.type != 'savings':
             self.type = 'checking'
             print(f'You chose an invalid account type, we have opened a checking account by default.')
 
+
         bank.append(self)
+
 
     def deposit(self, amount):
         self.balance += amount
@@ -55,6 +58,7 @@ class BankAccount():
         safeAccntNum = f'****{str(self.account_number)[slice(4, 8)]}'
         print(f'{self.name}\nAccount No.: {safeAccntNum}\nBalance: ${self.balance}\n')
 
+
 def monthly_interest(financial_institution):
     for account in financial_institution:
         account.add_interest()
@@ -63,27 +67,20 @@ def monthly_interest(financial_institution):
 
 kevin_accnt = BankAccount('Kevin McCarthy', 'savings')
 kevin_accnt.deposit(1000)
-# kevin_accnt.add_interest()
-# kevin_accnt.get_balance()
-# kevin_accnt.withdraw(2000)
-# kevin_accnt.print_statement()
+kevin_accnt.print_statement()
 
 mitchell_accnt = BankAccount('Mitchell', 'checking', 3141592)
 mitchell_accnt.deposit(400000)
-# mitchell_accnt.print_statement()
-# mitchell_accnt.add_interest()
-# mitchell_accnt.print_statement()
-# mitchell_accnt.withdraw(150)
-# mitchell_accnt.print_statement()
+mitchell_accnt.print_statement()
+mitchell_accnt.add_interest()
+mitchell_accnt.print_statement()
+mitchell_accnt.withdraw(150)
+mitchell_accnt.print_statement()
 
 emily_accnt = BankAccount('Emily Weyda')
 emily_accnt.deposit(38000)
-# emily_accnt.add_interest()
-# emily_accnt.get_balance()
-# emily_accnt.withdraw(2500)
-# emily_accnt.print_statement()
+emily_accnt.get_balance()
+emily_accnt.print_statement()
 
-# bank.append(kevin_accnt)
-# bank.append(mitchell_accnt)
-# bank.append(emily_accnt)
+
 monthly_interest(bank)
